f8b919617121ed1ca85c22bc0a2c03e77077106e
marcotulio95 <marco.tulio95@hotmail.com>
2017-05-31 16:06:39 -0300

    Update Switch para um exemplo mais simpleS

--- switch_case.rb
+++ switch_case.rb
@@ -1,16 +1,7 @@
-def switch(op = 'null')
-	case op
-		when '+' then puts  'Operacao de Soma'
-		when '-'  then puts 'Operacao de Subtracao'
-		when '*' then puts  'Operacao de Multiplicao'
-		when '/' then puts 'Operacao de Divisao'
-		else
-			puts 'Operacao nao encontrada'
-	end ## Fim do case	
-end
+op='*'
 
-switch ## Entrara no Else, imprimindo op nao encontrada
-switch('+') ## Opcionalmente, chama o switch com paranteses, Op soma sera impressa
-switch "-" ## Chama o switch sem paranteses, Op subtracao sera impressa
-switch "*"  ## Op Multiplicacao sera impressa
-switch "/" ## Op Divisao sera impressa
+case op
+	when '+','-' then puts  'Operacao Basica Encontrada'
+else
+	puts 'Operacao Basica nao encontrada'
+end ## Fim do case	
